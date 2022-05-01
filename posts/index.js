24d6e86eb7c8f4de9c01ab41bc33c266b5a53389
Harish <r.shivi@yahoo.com>
2022-05-01 19:46:17 +0530

    feat: add event bus

--- posts/index.js
+++ posts/index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const {randomBytes} =require('crypto')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(cors())
-
 app.use(bodyParser.json())
+
 const posts = {
 
 }
@@ -15,13 +16,27 @@ app.get('/posts', (req, res) => {
     res.send(posts)
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex')
     const {title} = req.body
     posts[id] = {
         id, title
     }
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, 
+            title
+        }
+    })
+
     res.status(201).send(posts[id])
+})
+
+app.post('/events', (req, res) => {
+    console.log('Received  Event in posts', {event: req.body})
+    res.send({})
 })
 
 app.listen(4000, () => {
